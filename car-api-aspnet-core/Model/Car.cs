ac12f6f26e9174d3af0c8700c74ddef0f130dbf4
Andrej Kastratovic <andrej.kastratovic@innovic.io>
2020-06-05 16:34:24 +0200

    Model validation

--- car-api-aspnet-core/Model/Car.cs
+++ car-api-aspnet-core/Model/Car.cs
@@ -1,21 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace car_api_aspnet_core.Model
 {
     public class Car
     {
         public int Id { get; set; }
         
+        [Required]
+        [StringLength(20, ErrorMessage = "Make field can not exceed 20 characters.")]
         public string Make { get; set; } 
         
+        [Required]
+        [StringLength(20, ErrorMessage = "Model field can not exceed 20 characters.")]
         public string Model { get; set; } 
         
+        [StringLength(15, ErrorMessage = "Color field can not exceed 15 characters.")]
         public string Color { get; set; } 
         
+        [Required]
         public string Price { get; set; } 
         
         public int Year { get; set; }
         
+        [Required]
+        [Range(0, 1)]
         public int Available { get; set; }
         
+        [Required]
         public int OwnerId { get; set; }
         
     }
--- car-api-aspnet-core/Model/Owner.cs
+++ car-api-aspnet-core/Model/Owner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using car_api_aspnet_core.Interfaces;
 
 namespace car_api_aspnet_core.Model
@@ -8,12 +9,18 @@ namespace car_api_aspnet_core.Model
     {
         public int Id { get; set; }
         
+        [Required]
+        [StringLength(50, ErrorMessage = "First name field can not exceed 50 characters.")]
         public string FirstName { get; set; }
         
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name field can not exceed 50 characters.")]
         public string LastName { get; set; }
         
+        [Range(18, 99)]
         public int Age { get; set; }
-
+        
+        [StringLength(100, ErrorMessage = "Address field can not exceed 100 characters.")]
         public string Address { get; set; }
     }
 }
